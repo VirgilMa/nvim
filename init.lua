df4fd5038ecf95e0ffbd0aac6fbb23f96bd970c8
virgilma <virgilma@tencent.com>
2022-10-08 14:31:47 +0800

    update

--- init.lua
+++ init.lua
@@ -1,10 +1,13 @@
-
 require('plugins')
 require('lsp')
 require('onedark').load()
 
 vim.wo.number = true
 vim.g.mapleader = "<Space>"
+
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
 
 -- print test
 vim.keymap.set('n', '<Space>ht', function()
