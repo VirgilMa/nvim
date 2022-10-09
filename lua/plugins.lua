8a2363c7554527e1a290ddc2d40411d802111536
virgilma <virgilma28@gmail.com>
2022-10-09 11:28:26 +0800

    add coc

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -29,13 +29,16 @@ return require('packer').startup(function(use)
 
   use 'navarasu/onedark.nvim' -- onedark theme
 
+  -- coc
+  use { 'neoclide/coc.nvim', branch = 'release' }
+
   -- complete
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
+  -- use 'hrsh7th/cmp-nvim-lsp'
+  -- use 'hrsh7th/cmp-buffer'
+  -- use 'hrsh7th/cmp-path'
+  -- use 'hrsh7th/cmp-cmdline'
+  -- use 'hrsh7th/nvim-cmp'
 
   -- tree-sitter
   use {
